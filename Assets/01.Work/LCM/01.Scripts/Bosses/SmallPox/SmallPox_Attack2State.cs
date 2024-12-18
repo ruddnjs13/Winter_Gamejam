5b8309040654c661950788628e39dbfc53b0b487
SUSOT <a01076754042@gmail.com>
2024-12-19 02:03:26 +0900

    Feat : SmallPox State

--- Assets/01.Work/LCM/01.Scripts/Bosses/SmallPox/SmallPox_Attack2State.cs
+++ Assets/01.Work/LCM/01.Scripts/Bosses/SmallPox/SmallPox_Attack2State.cs
@@ -2,6 +2,27 @@ using UnityEngine;
 
 public class SmallPox_Attack2State : BossState
 {
+    private Vector2 _moveDir;
     public SmallPox_Attack2State(SmallPox smallPox) : base(smallPox){
+    }
+
+    protected override void EnterState(){
+        _moveDir = _smallPox.TargetingPlayer;
+        
+    }
+
+    public override void FixedUpdateState(){
+        _smallPox.RbCompo.linearVelocity = _moveDir.normalized * _smallPox.SmallPoxData.moveSpeed;
+        if (_smallPox.Count == 0 && _smallPox.CheckStart)
+        {
+            Debug.Log("리턴으로 이동");
+            _smallPox.CheckStart = false;
+            _smallPox.TransitionState(BossStateType.Return);
+        }
+
+        if (_smallPox.Count != 0 && _smallPox.CheckStart)
+        {
+            _moveDir = _smallPox.TargetingPlayer;
+        }
     }
 }
