412b46f3e2946695406983ce0224644d0b557416
ruddnjs13 <ruddnjs013@gmail.com>
2024-12-19 21:31:28 +0900

    fix : Bullet

--- Assets/01.Work/LKW/01.Scripts/Weapon/Bullet.cs
+++ Assets/01.Work/LKW/01.Scripts/Weapon/Bullet.cs
@@ -5,7 +5,6 @@ public class Bullet : MonoBehaviour,IPoolable
 {
     private Rigidbody2D _rigidbody;
     
-    [SerializeField] private LayerMask _whatIsTarget;
     [SerializeField] private LayerMask _whatIsGround;
     [SerializeField] private int _moveSpeed;
     public string PoolName => objectPrefab.name;
@@ -18,22 +17,15 @@ public class Bullet : MonoBehaviour,IPoolable
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("충돌함");
-        if (collision.CompareTag("Boss"))
+        if (collision.CompareTag("Enemy")) return;
+        if (collision.CompareTag("Boss") || collision.CompareTag("Ground"))
         {
             PoolManager.Instance.Push(this);
         }
-        if ((0 << collision.gameObject.layer & _whatIsTarget) != 0)
+        else
         {
-            PoolManager.Instance.Push(collision as IPoolable);
             PoolManager.Instance.Push(this);
         }
-        else if (collision.CompareTag("Ground"))
-        {
-            Debug.Log("푸시");
-            PoolManager.Instance.Push(this);
-        }
-        PoolManager.Instance.Push(this);
     }
 
     public void SetVelocity(Vector2 velocity)
