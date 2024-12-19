ddf7347ba78d6d216ae6ade9b7a80ca75578f4c7
ruddnjs13 <ruddnjs013@gmail.com>
2024-12-19 23:23:07 +0900

    fix : Shoot

--- Assets/01.Work/LKW/01.Scripts/Player/TurnTrigger.cs
+++ Assets/01.Work/LKW/01.Scripts/Player/TurnTrigger.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Burst.Intrinsics;
 using UnityEngine;
 
@@ -8,7 +10,9 @@ public class TurnTrigger : MonoBehaviour
     [SerializeField] private bool _isVertical;
     public bool _isReverse;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private bool _canTurn = true;
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if ((1 << collision.gameObject.layer & _whatIsPlayer) != 0)
         {
@@ -18,6 +22,9 @@ public class TurnTrigger : MonoBehaviour
 
     private void PlayerTurn(Collider2D collision)
     {
+        if (!_canTurn) return;
+
+        StartCoroutine(TurnDelayCoroutine());
         GameObject playerObj = collision.gameObject;
         Player player = playerObj.GetComponent<Player>();
         player.isVerticalMove = _isVertical;
@@ -26,6 +33,17 @@ public class TurnTrigger : MonoBehaviour
         playerObj.transform.localRotation = Quaternion.Euler(Vector3.zero);
         playerObj.transform.localPosition = new Vector3(playerObj.transform.localPosition.x
             , 0.94f, playerObj.transform.localPosition.z);
-        
+    }
+
+    private IEnumerator DelayAndTurn()
+    {
+        yield return new WaitForSeconds(0.2f);
+    }
+
+    private IEnumerator TurnDelayCoroutine()
+    {
+        _canTurn = false;
+        yield return new WaitForSeconds(0.2f);
+        _canTurn = true;
     }
 }
